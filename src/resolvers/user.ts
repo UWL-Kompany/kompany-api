13e44fc189d94bc50ce5bf1a7ea2bbbd9b24494c
RubyFalcon <tupai@live.co.uk>
2021-03-12 14:05:49 +0000

    base authentication server setup for user

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -50,7 +50,9 @@ export class UserResolver {
         ],
       };
     }
-    const userId = await redis.get(FORGET_PASSWORD_PREFIX + token);
+
+    const key = FORGET_PASSWORD_PREFIX + token;
+    const userId = await redis.get(key);
     if (!userId) {
       return {
         errors: [
@@ -75,6 +77,7 @@ export class UserResolver {
     }
     user.password = await argon2.hash(newPassword);
     await em.persistAndFlush(user);
+    await redis.del(key);
     //login user after changing pass
     req.session.userId = user.id;
 
